0573d0f51c10192ffbfd301ca8e73d0de5b6ea35
Alexandr Popolitov <popolit@gmail.com>
2015-01-31 21:45:28 +0100

    Towards readtable-based approach to TeX reader

--- l33tex.lisp
+++ l33tex.lisp
@@ -100,7 +100,76 @@
     (iter->list (mk-pushable-iter (mk-simple-chars (mk-prereader stream))))))
 	
 
+(defclass teh-reader (over-iter)
+  ((entry-point :initform (error "You should set the entry point, otherwise I won't work!"))))
 
+(defparameter category-numbers
+  '((:escape . 0)
+    (:begin-group . 1)
+    (:end-group . 2)
+    (:math-shift . 3)
+    (:alignment-tab . 4)
+    (:end-of-line . 5)
+    (:parameter . 6)
+    (:superscript . 7)
+    (:subscript . 8)
+    (:ignored . 9)
+    (:space . 10)
+    (:letter . 11)
+    (:other . 12)
+    (:active . 13)
+    (:comment . 14)
+    (:invalid . 15)))
+     
+(defclass tex-token ()
+  ((text :initarg :text :initform "")
+   (cat :initarg :cat :initform 12)))
+
+(defun other-token (char)
+  (make-instance 'tex-token :text char :cat :other))
+
+(defparameter teh-readtable (make-hash-table :test #'equal))
+
+(defun get-char-reader (char)
+  (gethash char teh-readtable))
+(defun set-char-reader (char fun)
+  (setf (gethash char teh-readtable) fun))
+
+(defun get-char-cat-reader (char)
+  (get-cat-reader (get-char-cat char)))
+
+(defparameter char-cats (make-hash-table :test #'equal))
+
+(defun get-char-cat (char)
+  (gethash char char-cats :other))
+(defun set-char-cat (char cat)
+  (setf (gethash char char-cats) cat))
+
+(defparameter cat-readtable (make-hash-table :test #'equal))
+
+(defun get-cat-reader (cat)
+  (gethash cat cat-readtable))
+(defun set-cat-reader (cat fun)
+  (setf (gethash cat cat-readtable) fun))
+
+(defun other-cat-reader (char iter)
+  (declare (ignore iter))
+  (other-token char))
+
+(defun vanilla-reader (iter)
+  (with-slots (sub-iterator) iter
+    (let ((new-char (next-iter sub-iterator)))
+      (let ((char-reader (get-char-reader new-char)))
+	(if (not char-reader)
+	    (let ((cat-reader (get-char-cat-reader new-char)))
+	      (if (not cat-reader)
+		  (error "Every character is supposed to have a category, so we shouldn't be here!")
+		  (funcall cat-reader new-char iter)))
+	    (funcall char-reader new-char iter))))))
+
+
+(defmethod next-iter ((iter teh-reader))
+  (funcall (slot-value iter 'entry-point) iter))
 
 ;; (define-condition valuable-condition (error)
 ;;   ((value :initarg :value :reader valuable-value)))
